446e966873fa3ecb6368caf413b8f98f88e0e198
w9895963 <12085367@163.com>
2023-01-12 02:32:08 +0800

    ⬇

---
+++ Assets/Scripts/TempProjects/EventData/DataName/DataNameD_TypeDict.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EventData
+{
+
+
+
+    public static partial class DataNameD
+    {
+
+        //字典:类型判断正则表达
+        public static Dictionary<System.Type, string> TypeDict = new Dictionary<System.Type, string>(){
+            {typeof(Vector2), @"(向量|施力|法线)$"},
+            {typeof(bool), @"^是否"},
+            {typeof(GameObject), @"物体$"},
+            {typeof(float), @"值$"}
+        };
+
+
+    }
+
+
+}
--- Assets/Scripts/TempProjects/EventData/DataName/DataNameF.cs
+++ Assets/Scripts/TempProjects/EventData/DataName/DataNameF.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -6,48 +5,6 @@ using UnityEngine;
 
 namespace EventData
 {
-    //枚举 ：数据名
-    public enum DataName
-    {
-        全局_输入_移动向量,
-        全局_输入_跳跃值,
-        全局_输入_冲刺值,
-
-        运动速度向量,
-
-
-        重力向量,
-        地表法线,
-        是否站在地面,
-        地面物体,
-
-
-        行走施力,
-        跳跃施力,
-        重力施力,
-    }
-
-
-
-    //预输入参数
-    public static partial class DataNameD
-    {
-       
-        //字典:类型判断正则表达
-        public static Dictionary<System.Type, string> TypeRegexDic = new Dictionary<System.Type, string>(){
-            {typeof(Vector2), @"(向量|施力|法线)$"},
-            {typeof(bool), @"^是否"},
-            {typeof(GameObject), @"物体$"},
-            {typeof(float), @"值$"}
-        };
-
-
-    }
-
-
-
-
-
     //数据名方法
     public static class DataNameF
     {
@@ -80,7 +37,7 @@ namespace EventData
             //~创建一个正则表达式⁡
             string pattern = null;
             //获得和类型对应的正则表达式
-            bool success = DataNameD.TypeRegexDic.TryGetValue(type, out pattern);
+            bool success = DataNameD.TypeDict.TryGetValue(type, out pattern);
             if (!success)
             {
                 Debug.LogError($"类型[{type}]没有预设对应的正则表达式");
@@ -110,7 +67,7 @@ namespace EventData
             System.Type type = default;
 
             //历遍正则字典TypeRegexDic
-            DataNameD.TypeRegexDic.ForEach((keyValue) =>
+            DataNameD.TypeDict.ForEach((keyValue) =>
             {
                 //创建一个正则表达式
                 Regex r = new Regex(keyValue.Value);
