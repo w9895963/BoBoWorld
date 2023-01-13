40b8a796df0c7997c12ac39165ed8144b3e34eb4
w9895963 <12085367@163.com>
2023-01-13 18:54:00 +0800

    ConfigureItem_GetPhysicData

--- Assets/Scripts/TempProjects/Configure/ConfigureItem/ConfigureItem_GetPhysicData.cs
+++ Assets/Scripts/TempProjects/Configure/ConfigureItem/ConfigureItem_GetPhysicData.cs
@@ -82,10 +82,85 @@ namespace Configure
 
         }
 
-
-
+        [System.Serializable]
+        [AddTypeMenu("物理/获取物理量")]
         public class ConfigureItem_GetPhysicData_ : ConfigureBase_
         {
+
+
+            [NaughtyAttributes.InfoBox("无参数", EInfoBoxType.Normal)]
+
+            //脚本说明
+            [NaughtyAttributes.Label("其他信息")]
+            public ShowOnlyText info = new ShowOnlyText("从Unity组件中获得物理数据:", "运动速度");
+
+            //必要组件
+            protected override List<Type> requiredTypes => new List<Type>() { typeof(Rigidbody2D) };
+
+
+            public ConfigureItem_GetPhysicData_()
+            {
+
+                Construct();
+            }
+
+
+
+
+
+
+
+
+            private GameObject gameObject;
+            private EventDataHandler<Vector2> speedD;
+            private Rigidbody2D rigidbody2D;
+
+            private void Construct()
+            {
+                //创建运行器
+                ConfigureRunner runner = new ConfigureRunner(initialize, enable, disable, destroy);
+
+                createRunner = (obj) =>
+                {
+                    gameObject = obj;
+                    return runner;
+                };
+
+
+
+
+
+                //获取物理组件
+                rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+            }
+            private void initialize()
+            {
+                //获取运动速度
+                speedD = EventDataF.GetData<Vector2>(DataName.运动速度向量, gameObject);
+            }
+            private void destroy()
+            {
+
+            }
+            private void enable()
+            {
+                BasicEvent.OnFixedUpdate.Add(gameObject, FixedUpdate);
+            }
+
+            private void disable()
+            {
+                BasicEvent.OnFixedUpdate.Remove(gameObject, FixedUpdate);
+            }
+
+
+
+            private void FixedUpdate()
+            {
+                speedD.Data = rigidbody2D.velocity;
+            }
+
+
+
 
         }
     }
