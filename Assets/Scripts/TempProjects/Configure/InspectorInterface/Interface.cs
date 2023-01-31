8dda278326f5afb48c25133a1dd3a31bf8950594
w9895963 <12085367@163.com>
2023-01-31 20:21:24 +0800

    InspectorInterface:暂时改到这,还无法用

---
+++ Assets/Scripts/TempProjects/Configure/InspectorInterface/Interface.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Configure.InspectorInterface.InterfaceItems;
+using UnityEngine;
+
+namespace Configure.InspectorInterface
+{
+
+    ///<summary>配置参数表</summary>
+    [Serializable]
+    public class ParamsTable
+    {
+
+        private List<ParmData> datas = new List<ParmData>();
+
+        // [SerializeField]
+        // [SerializeReference]
+        // private List<InterfaceItem> interfaceItems;
+        public string name;
+
+
+
+
+        public ParamsTable(params ParmData[] presetData)
+        {
+            datas.AddRange(presetData);
+            Init();
+        }
+
+
+
+
+        public void Init()
+        {
+            //根据类型创建对应的 InterfaceItem<T>
+            // if (datas.Count > 0)
+            //     DebugF.GetClassFieldsLog(datas[0]).Log();
+
+
+        }
+
+        public void GetData(int index)
+        {
+
+        }
+
+
+
+
+
+    }
+
+    public abstract class ParmData
+    {
+        public string showName;
+        public bool useStaticData = true;
+        public string dataName;
+        public string help;
+        public abstract System.Object data { get; set; }
+    }
+    public class ParmData<T> : ParmData
+    {
+        public T dataT;
+
+        public ParmData(string showName, bool useStaticData, string dataName = null, T data = default, string help = null)
+        {
+            this.showName = showName;
+            this.useStaticData = useStaticData;
+            this.dataName = dataName;
+            this.dataT = data;
+            this.help = help;
+        }
+
+
+        public override object data { get => dataT; set => dataT = (T)value; }
+    }
+
+
+
+
+
+}
+
+namespace Configure.InspectorInterface.InterfaceItems
+{
+    public class InterfaceItem
+    {
+        [HideInInspector]
+        public string name;
+
+    }
+
+    [Serializable]
+    public class InterfaceItem<T> : InterfaceItem
+    {
+        [NaughtyAttributes.AllowNesting]
+        [NaughtyAttributes.Label("值")]
+        public T value;
+
+    }
+
+    [Serializable]
+    public class InterfaceItemInt : InterfaceItem<int>
+    {
+    }
+    [Serializable]
+    public class InterfaceItemListInt : InterfaceItem<List<int>>
+    {
+    }
+
+    [Serializable]
+    public class InterfaceItemDataNameDropList : InterfaceItem
+    {
+        [NaughtyAttributes.AllowNesting]
+        [NaughtyAttributes.Dropdown(nameof(DataNameDropList))]
+        [NaughtyAttributes.Label("")]
+        public string value;
+
+        private Type type;
+
+        public InterfaceItemDataNameDropList(Type type)
+        {
+            this.type = type;
+        }
+
+
+        private string[] DataNameDropList()
+        {
+            return EventData.DataNameF.GetAllNamesOnType(type);
+        }
+    }
+}
