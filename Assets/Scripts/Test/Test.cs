4a5645975d012a67725be591fe61e626efcc78b2
w9895963 <12085367@163.com>
2023-02-03 20:08:41 +0800

    1

--- Assets/Scripts/Test/Test.cs
+++ Assets/Scripts/Test/Test.cs
@@ -28,24 +28,27 @@ public class Test : MonoBehaviour
 
     public void 本地测试方法()
     {
-        conditionTrigger.GetCondition(gameObject).Log(logFields: false);
-
+        conditionTrigger.GetCondition(gameObject).Log("conditionTrigger");
+        
 
     }
 
 
-
-    [UnityEditor.MenuItem("测试/测试方法")]
     public static void 测试方法()
     {
-        GameObject.FindObjectOfType<Test>().本地测试方法();
+
     }
 
 
-    [UnityEditor.MenuItem("测试/执行所有本地测试方法")]
-    public static void 执行所有本地测试方法()
+
+    public static class TestMethodInMenu
     {
-        GameObject.FindObjectsOfType<Test>().ForEach(obj => obj.本地测试方法());
+        [UnityEditor.MenuItem("测试/测试方法")]
+        public static void 测试方法()
+        {
+            GameObject.FindObjectsOfType<Test>().ForEach(obj => obj.本地测试方法());
+            Test.测试方法();
+        }
     }
 
 }
